a4223c1e5a03ee4d27f42eaba88085f8f3e9e9f9
CWX <792421222@qq.com>
2018-07-27 12:51:27 +0800

    waterfall

---
+++ pages/index/index.js
@@ -0,0 +1,64 @@
+
+Page({
+  data:{
+   goodsArray: [
+      {
+        id: 1,
+        image: '../../images/123.png',
+        name: 'the name',
+        price: '1000'
+      },
+       {
+        id: 1,
+        image: '../../images/123.png',
+        name: '这是一段物品名称这是一段物品名称这是一段物品名称这是一段物品名称这是一段物品名称这是一段物品名称这是一段物品名称这是一段物品名称',
+        price: '1000'
+      },
+       {
+        id: 1,
+        image: '../../images/123.png',
+        name: 'the name',
+        price: '1000'
+      },
+       {
+        id: 1,
+        image: '../../images/123.png',
+        name: 'the name',
+        price: '1000'
+      },
+       {
+        id: 1,
+        image: '../../images/123.png',
+        name: '这是一段物品名称这是一段物品名称这是一段物品名称',
+        price: '1000'
+      },
+       {
+        id: 1,
+        image: '../../images/123.png',
+        name: 'the name',
+        price: '1000'
+      },
+       {
+        id: 1,
+        image: '../../images/123.png',
+        name: 'the name',
+        price: '1000'
+      },
+       {
+        id: 1,
+        image: '../../images/123.png',
+        name: 'the name',
+        price: '1000'
+      },
+       {
+        id: 1,
+        image: '../../images/123.png',
+        name: 'the name',
+        price: '1000'
+      }
+   ]
+  },
+  methods:{
+
+  }
+})
